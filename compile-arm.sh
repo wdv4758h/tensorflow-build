4293b7b152761a5c6c64a5a700a57acf359174cc
Chiu-Hsiang Hsu <wdv4758h@gmail.com>
2017-04-20 14:28:21 +0800

    Update ARM toolchain settings

--- compile-arm.sh
+++ compile-arm.sh
@@ -2,29 +2,23 @@
 
 TARGET=target
 TOOLS_DIR=tools
-TOOLCHAIN_ROOT="arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf"
-#TOOLCHAIN_ROOT="arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64"
+TOOLCHAIN_ROOT=""
 TOOLCHAIN_NAME=arm-linux-gnueabihf
 
 VERSION=$1
 
-if [ -z $VERSION ] 
+if [ -z $VERSION ]
 then
 	echo "please provide the version tag to build"
 	exit 1
-fi	
+fi
 
-mkdir $TARGET
+mkdir -p $TARGET
 
 CURRENT_DIR=$(pwd)
 
-git clone https://github.com/raspberrypi/tools "$TARGET/$TOOLS_DIR" --depth 1
+# Get Toolchain
+# TODO
 
-./cross-compile.sh $CURRENT_DIR/$TARGET/$TOOLS_DIR/$TOOLCHAIN_ROOT $TOOLCHAIN_NAME $VERSION
-
-
-
-
-
-
-
+# Start Compile (toolchain root, toolchain name, tensorflow version)
+./cross-compile.sh /usr $TOOLCHAIN_NAME $VERSION
