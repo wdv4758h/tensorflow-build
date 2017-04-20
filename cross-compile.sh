ca4806f29798fcdd1c8e541bf9dcf6d8af989281
Chiu-Hsiang Hsu <wdv4758h@gmail.com>
2017-04-20 14:32:46 +0800

    Update cross compile settings

--- cross-compile.sh
+++ cross-compile.sh
@@ -14,10 +14,10 @@ then
 	echo "usage: $0 <absolute cross toolchain path> <toolchain prefix> <tensorflow tag/commit>"
 	echo "seaching gcc here : $GCC"
 	exit
-fi 
+fi
 
 GCC_VERSION=$($GCC -dumpversion)
-echo "using gcc : $GCC version $GCC_VERSION" 
+echo "using gcc : $GCC version $GCC_VERSION"
 
 PATCH_NAME=tf-crosscompile-$CT_NAME.patch
 mkdir $BUILD_DIR
@@ -30,9 +30,13 @@ sed -i "s#%%CT_NAME%%#$CT_NAME#g" $PATCH_NAME
 sed -i "s#%%CT_ROOT_DIR%%#$CT_DIR#g" $PATCH_NAME
 sed -i "s#%%CT_GCC_VERSION%%#$GCC_VERSION#g" $PATCH_NAME
 
-git clone https://github.com/tensorflow/tensorflow.git
-
+mkdir -p tensorflow
 cd tensorflow
+git init
+git remote add origin https://github.com/tensorflow/tensorflow.git
+git fetch
+git reset HEAD --hard
+git clean -f -d
 
 git checkout $TENSORFLOW_VERSION || exit 1
 
@@ -44,4 +48,4 @@ yes ''|./configure || exit 1
 
 echo "launching bazel with flags '$BAZEL_FLAGS'"
 
-bazel build $BAZEL_FLAGS -c opt --copt="-march=armv6" --copt="-mfpu=vfp" --copt="-funsafe-math-optimizations" --copt="-ftree-vectorize" --copt="-fomit-frame-pointer"  tensorflow:libtensorflow.so --cpu=armeabi --crosstool_top=//tools/arm_compiler:toolchain --verbose_failures
+bazel build $BAZEL_FLAGS -c opt --copt="-march=armv7l" --copt="-mfpu=vfp" --copt="-funsafe-math-optimizations" --copt="-ftree-vectorize" --copt="-fomit-frame-pointer" //tensorflow/tools/pip_package:build_pip_package  --cpu=armeabi --crosstool_top=//tools/arm_compiler:toolchain --verbose_failures
