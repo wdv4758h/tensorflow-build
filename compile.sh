6ff77b64c9d57bf4933cbe929f6996111cbd9341
Chiu-Hsiang Hsu <wdv4758h@gmail.com>
2017-04-20 14:55:57 +0800

    Add TensorFlow settings

--- compile.sh
+++ compile.sh
@@ -20,7 +20,49 @@ cd tensorflow
 
 git checkout $TENSORFLOW_VERSION || exit 1
 
-yes ''|./configure || exit 1
+########################################
+# TensorFlow Settings (this will apply automatically by TensorFlow's configure)
+
+export PYTHON_BIN_PATH=/usr/bin/python3
+export USE_DEFAULT_PYTHON_LIB_PATH=1
+
+export TF_NEED_CUDA=0
+
+## CUDA
+#export TF_NEED_CUDA=1
+#export GCC_HOST_COMPILER_PATH=/usr/bin/gcc
+#export TF_UNOFFICIAL_SETTING=1
+#export CUDA_TOOLKIT_PATH=/usr/local/cuda-8.0/
+## export CUDA_TOOLKIT_PATH=/usr/local/cuda-6.5/
+#export TF_CUDA_VERSION=$($CUDA_TOOLKIT_PATH/bin/nvcc --version | sed -n 's/^.*release \(.*\),.*/\1/p')
+#export TF_CUDA_COMPUTE_CAPABILITIES=3.2
+#
+## cuDNN
+## /usr/include/x86_64-linux-gnu/cudnn_v6.h
+#export CUDNN_INSTALL_PATH=/usr/lib/x86_64-linux-gnu/
+#export TF_CUDNN_VERSION=$(sed -n 's/^#define CUDNN_MAJOR\s*\(.*\).*/\1/p' /usr/include/x86_64-linux-gnu/cudnn_v6.h)
+
+# disable Google Cloud Platform support
+export TF_NEED_GCP=0
+# disable Hadoop File System support
+export TF_NEED_HDFS=0
+# disable OpenCL support
+export TF_NEED_OPENCL=0
+# disable XLA JIT compiler
+export TF_ENABLE_XLA=0
+# enable jemalloc support
+export TF_NEED_JEMALLOC=1
+# set up architecture dependent optimization flags
+# export CC_OPT_FLAGS="-march=native"
+export CC_OPT_FLAGS=""
+
+# make sure the proxy variables are in all caps, otherwise bazel ignores them
+export HTTP_PROXY=`echo $http_proxy | sed -e 's/\/$//'`
+export HTTPS_PROXY=`echo $https_proxy | sed -e 's/\/$//'`
+
+########################################
+
+./configure
 
 echo "launching bazel with flags '$BAZEL_FLAGS'"
 
